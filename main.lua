76d6ff15dcded65c3a93b5255113f5fbe74dc766
Tachytaenius <henryfthomson@gmail.com>
2024-01-01 12:43:44 +0000

    Update codebase

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 local vec2 = require("lib.mathsies").vec2
 
+local tau = math.pi * 2
+
 local dummy = love.graphics.newImage(love.image.newImageData(1, 1))
 local shader = love.graphics.newShader("shader.glsl")
 
@@ -7,6 +9,64 @@ local maxSpeed = 100
 local maxAccel = 50
 local accelCurveShaper = 1.5
 local acceleration = vec2(maxAccel, 0)
+
+local dotVelocity = vec2(0, 0)
+local speedRegulationHarshness = 10e-10
+local speedRegulationMultiplier = 1 - speedRegulationHarshness
+
+local function normaliseOrZero(v)
+	local l = #v
+	if l == 0 then
+		return vec2.clone(v)
+	end
+	return vec2.normalise(v)
+end
+
+local function setVectorLength(v, l)
+	return normaliseOrZero(v) * l
+end
+
+local function sign(x)
+	if x > 0 then
+		return 1
+	elseif x < 0 then
+		return -1
+	end
+	return 0
+end
+
+local function getAccelerationMultiplier(velocity, accelerationDirection, maxSpeed, accelCurveShaper)
+	if accelerationDirection == 0 then
+		return 0
+	end
+	local function getAccelerationMultiplierCore(speed, accelerationDirection)
+		-- Speed can't be negative, and accelerationDirection should be negated (whether that's positive or negative) if velocity was too
+		if accelerationDirection <= 0 then
+			return 1
+		end
+		return ((maxSpeed - speed) / maxSpeed) ^ (1 / accelCurveShaper)
+	end
+	if velocity > -maxSpeed and velocity <= 0 then
+		return getAccelerationMultiplierCore(-velocity, -accelerationDirection)
+	elseif velocity >= 0 and velocity < maxSpeed then
+		return getAccelerationMultiplierCore(velocity, accelerationDirection)
+	elseif sign(velocity) * sign(accelerationDirection) == 1 then
+		-- If you're trying to accelerate in the same direction you're moving and abs(vel) >= maxSpeed then no movement
+		return 0
+	else
+		return 1
+	end
+end
+
+local function multiplyVectorInDirection(v, a, m)
+	local vRotated = vec2.rotate(v, -a)
+	vRotated.x = vRotated.x * m
+	return vec2.rotate(vRotated, a)
+end
+
+local function shortestAngleDifference(a, b)
+	return (a - b + tau / 2) % tau - tau / 2
+end
 
 function love.update(dt)
 	local accelerationChangeRate = 100
@@ -26,6 +86,37 @@ function love.update(dt)
 	if #accelerationChange > 0 then
 		acceleration = acceleration + vec2.normalise(accelerationChange) * accelerationChangeRate * dt
 	end
+
+	-- Accelerate dot
+	if love.keyboard.isDown("1") then
+		dotVelocity = vec2(0, 0)
+	elseif love.keyboard.isDown("2") then
+		dotVelocity = vec2(maxSpeed * 2, 0)
+	end
+	local accelerationVector
+	if #dotVelocity > 0 then
+		local velocityAngle = vec2.toAngle(dotVelocity)
+		local accelRotated = vec2.rotate(acceleration, -velocityAngle)
+		local multiplier = getAccelerationMultiplier(#dotVelocity, accelRotated.x, maxSpeed, accelCurveShaper)
+		accelerationVector = multiplyVectorInDirection(acceleration, velocityAngle, multiplier)
+	else
+		accelerationVector = acceleration
+	end
+	-- If acceleration would increase speed while being over max speed, cap it to previous speed or max speed, whichever is higher. Preserves direction
+	local attemptedDelta = accelerationVector * dt
+	local attemptedNewVelocity = dotVelocity + attemptedDelta
+	local finalDelta, finalNewVelocity
+	if #attemptedNewVelocity > maxSpeed and #attemptedNewVelocity > #dotVelocity then
+		finalNewVelocity = setVectorLength(attemptedNewVelocity, math.max(maxSpeed, #dotVelocity) * speedRegulationMultiplier)
+		finalDelta = finalNewVelocity - dotVelocity
+		-- #finalDelta may be larger than #attemptedDelta
+		assert(#finalNewVelocity <= #attemptedNewVelocity, "Attempting to reduce speed below speed dot shouldn't go above resulted in an increase in speed")
+	else
+		finalDelta = attemptedDelta
+		finalNewVelocity = attemptedNewVelocity
+	end
+	-- Divide finalDelta by dt to get final acceleration
+	dotVelocity = finalNewVelocity
 end
 
 function love.draw()
@@ -35,12 +126,17 @@ function love.draw()
 	shader:send("acceleration", {vec2.components(acceleration)})
 	love.graphics.draw(dummy, 0, 0, 0, love.graphics.getDimensions())
 	love.graphics.setShader()
-	love.graphics.setColor(0, 0, 0)
+	love.graphics.setLineWidth(3)
 	love.graphics.circle("line", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, maxSpeed)
 	-- love.graphics.circle("line", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, maxAccel)
 	love.graphics.line(
 		love.graphics.getWidth() / 2, love.graphics.getHeight() / 2,
 		love.graphics.getWidth() / 2 + acceleration.x, love.graphics.getHeight() / 2 + acceleration.y
 	)
-	love.graphics.setColor(1, 1, 1)
+	love.graphics.setPointSize(6)
+	love.graphics.points(vec2.components(vec2(love.graphics.getDimensions()) / 2 + dotVelocity))
+	love.graphics.print(
+		"Point speed: " .. #dotVelocity .. "\n" ..
+		"Angle diff of accel and point vel: " .. shortestAngleDifference(vec2.toAngle(dotVelocity), vec2.toAngle(acceleration))
+	)
 end
--- readme.md
+++ readme.md
@@ -1,12 +1,25 @@
 # Racer Acceleration Visualisation
 
 Imagine a racing machine that has a top speed (acceleration drops off as you start to push into it).
-For any arbitrary acceleration from its engine and any arbitrary velocity it is moving at, what acceleration would it experience?
+For any arbitrary attempted acceleration from its engine and any arbitrary velocity it is moving at, what acceleration would it experience?
 
 Origin is in centre of screen.
-Black circle is max speed.
-Black line is attempted acceleration vector.
+Circle is max speed.
+Line is attempted acceleration vector.
 XY pos of pixel around origin is velocity of machine.
 R and G colour channels of pixel correspond to X and Y of damped acceleration (acceleration multiplied by a number in [0, 1] in direction of velocity).
 
+White dot is an object's velocity, following the damped accelerations.
+Notice how the dot tends to move along the max speed circle when accelerating in a new direction while the dot is at max speed, unless it's a large enough change in direction to pull the dot off the circle.
+
+The code to accelerate the dot's velocity gets the acceleration from the function, but it also disables accelerating beyond max speed, or if it is already beyond max speed, any further than current speed (considering the function in a purer mathematical sense without the precision limitations of computers, it may not actually have the problem of accelerating (slowly) over max speed. It might just be due to how it's calculated).
+The way limiting the speed is achieved is by checking that the attempted new velocity (`currentVelocity + accelerationFromFunction * deltaTime`) this frame is above max speed, and if it is, trimming the new velocity's length to `max(maxSpeed, lengthOfCurrentVelocity) * speedRegulationMultiplier`.
+(`lengthOfCurrentVelocity` is not to be confused with the length of the attempted new velocity.)
+Without the multiplier, precision problems cause continued increase in speed.
+`speedRegulationMultiplier` is a number a very small amount below 1, which drags above-max-speed velocities backwards a bit, so that if your velocity's magnitude does change, it's decreasing.
+If the `max` function is replaced with just the length of the current velocity, the speed is less static at max speed.
+This is all for a racing machine without considering any other slowdown forces.
+
 WASD to move acceleration vector.
+1 to reset dot velocity to origin.
+2 to set dot velocity to twice max speed in the +x direction.
